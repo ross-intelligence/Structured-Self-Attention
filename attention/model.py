152bd27d1c990172d19678a3f7c401655ca02e38
Sid <thota@Rossintelligence.com>
2018-10-25 09:38:13 -0400

    nli: model not done yet

--- attention/model.py
+++ attention/model.py
@@ -7,13 +7,13 @@ import torch.utils.data as data_utils
 
 dtype = torch.cuda
 
-class StructuredSelfAttention(torch.nn.Module):
+class StructuredSelfAttentionForNLI(torch.nn.Module):
     """
     The class is an implementation of the paper A Structured Self-Attentive Sentence Embedding including regularization
-    and without pruning. Slight modifications have been done for speedup
+    and without pruning. Slight modifications have been done for speedup. 
     """
    
-    def __init__(self,batch_size,lstm_hid_dim,d_a,r,max_len,emb_dim=100,vocab_size=None,use_pretrained_embeddings = False,embeddings=None,type=0,n_classes = 1):
+    def __init__(self,batch_size,lstm_hid_dim,d_a,r,max_len,emb_dim=100,vocab_size=None,type=0,n_classes = 1):
         """
         Initializes parameters suggested in paper
  
@@ -38,7 +38,6 @@ class StructuredSelfAttention(torch.nn.Module):
         """
         super(StructuredSelfAttention,self).__init__()
        
-        self.embeddings,emb_dim = self._load_embeddings(use_pretrained_embeddings,embeddings,vocab_size,emb_dim)
         self.lstm = torch.nn.LSTM(emb_dim,lstm_hid_dim,1,batch_first=True)
         self.linear_first = torch.nn.Linear(lstm_hid_dim,d_a)
         self.linear_first.bias.data.fill_(0)
@@ -53,26 +52,6 @@ class StructuredSelfAttention(torch.nn.Module):
         self.r = r
         self.type = type
                  
-    def _load_embeddings(self,use_pretrained_embeddings,embeddings,vocab_size,emb_dim):
-        """Load the embeddings based on flag"""
-       
-        if use_pretrained_embeddings is True and embeddings is None:
-            raise Exception("Send a pretrained word embedding as an argument")
-           
-        if not use_pretrained_embeddings and vocab_size is None:
-            raise Exception("Vocab size cannot be empty")
-   
-        if not use_pretrained_embeddings:
-            word_embeddings = torch.nn.Embedding(vocab_size,emb_dim,padding_idx=0)
-            
-        elif use_pretrained_embeddings:
-            word_embeddings = torch.nn.Embedding(embeddings.size(0), embeddings.size(1))
-            word_embeddings.weight = torch.nn.Parameter(embeddings)
-            emb_dim = embeddings.size(1)
-            
-        return word_embeddings,emb_dim
-       
-        
     def softmax(self,input, axis=1):
         """
         Softmax applied to axis=n
